813e4c4dfd84f7c2aef29d3a3096406eeb3a7825
anderson falconi melo <falconi_fal@hotmail.com>
2023-08-22 09:14:27 -0300

    feat: Add Features Slice

---
+++ src/components/Icons/BargraphIcon.tsx
@@ -0,0 +1,37 @@
+function BargraphIcon() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="80"
+        height="80"
+        viewBox="0 0 80 80"
+        fill="none"
+      >
+        <path
+          d="M73.3333 73.3333H6.66663"
+          stroke="#0891B2"
+          strokeWidth="4.99091"
+          stroke-linecap="round"
+        />
+        <path
+          d="M70 73.3333V48.3333C70 45.5719 67.7614 43.3333 65 43.3333H55C52.2386 43.3333 50 45.5719 50 48.3333V73.3333"
+          stroke="#0891B2"
+          strokeOpacity="0.5"
+          strokeWidth="4.99091"
+        />
+        <path
+          d="M50 73.3333V16.6667C50 11.9526 50 9.59559 48.5355 8.13112C47.0711 6.66666 44.714 6.66666 40 6.66666C35.286 6.66666 32.9289 6.66666 31.4645 8.13112C30 9.59559 30 11.9526 30 16.6667V73.3333"
+          stroke="#0891B2"
+          strokeWidth="4.99091"
+        />
+        <path
+          d="M30 73.3333V31.6667C30 28.9052 27.7614 26.6667 25 26.6667H15C12.2386 26.6667 10 28.9052 10 31.6667V73.3333"
+          stroke="#0891B2"
+          strokeOpacity="0.5"
+          strokeWidth="4.99091"
+        />
+      </svg>
+    );
+}
+
+export default BargraphIcon;
